4dcc10d556ee2b6dc7ef93de568021f6db66a4a1
Shreyansh Agrawal <ashreyansh47@gmail.com>
2023-01-13 11:09:11 +0530

    saved till layout

--- lib/Screens/home_profile.dart
+++ lib/Screens/home_profile.dart
@@ -1,20 +1,29 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Spacer(),
-        Center(
-          child: Text("Hey at Profile page "),
-        ),
-        Spacer()
-      ],
+    return SafeArea(
+      child: Column(
+        children: [SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Spacer(),
+               
+                Icon(
+                Icons.settings  ,
+                  size: 30,
+                )
+              ],
+            )
+        ],
+      ),
     );
   }
 }
--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -4,8 +4,9 @@ import 'package:google_fonts/google_fonts.dart';
 
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
-      appBarTheme: AppBarTheme(backgroundColor: primary, elevation: 0),
-      textTheme: GoogleFonts.latoTextTheme());
+        appBarTheme: AppBarTheme(backgroundColor: primary, elevation: 0),
+        textTheme: GoogleFonts.latoTextTheme(),
+      );
 
   static Color primary = Color(0xFF529A80);
   static Color text1 = Colors.black;
@@ -15,7 +16,7 @@ class MyTheme {
   static Color containerColorSecondary = Colors.grey.shade200;
   static Color icon1 = Colors.white;
   static Color icon2 = Colors.grey;
-  static Color icon3 = Colors.black; 
+  static Color icon3 = Colors.black;
   static Color grey = Colors.grey;
   static Color black = Colors.black;
   static Color white = Colors.white;
